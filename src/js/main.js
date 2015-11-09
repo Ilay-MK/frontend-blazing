b845781753a2d585bf718c0d903643cd59ce599b
Ilya Purouski <MiKrob09@gmail.com>
2015-11-10 00:17:20 +0300

    Создал вложенность в js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 $(function(){
     console.log('in main.js! ');
 })
+/*
+ Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.min.js
+
+/*
+    Custom
+ */
+//= partials/plugins.js
--- src/js/plugins.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-    console.log('in plugins.js! ');
-})
